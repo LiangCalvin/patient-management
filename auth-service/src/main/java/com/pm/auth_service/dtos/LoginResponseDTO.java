39b0ce5ae26c090d3e0646e73c7f294ff6ae57c8
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-05-18 14:53:24 +0700

    add Dto request and response.

---
+++ auth-service/src/main/java/com/pm/auth_service/dtos/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.pm.auth_service.dtos;
+
+public class LoginResponseDTO {
+    private final String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
